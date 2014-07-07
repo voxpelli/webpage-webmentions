ecacf088f080c3e4436950ba780d8baae8e4162f
Pelle Wessman <pelle@kodfabrik.se>
2014-07-07 20:29:07 +0200

    Update content of normalized column

---
+++ migrations/20140707202701_update-normalized-column.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var urlTools = require('../lib/utils/url-tools');
+
+exports.down = exports.up = function(knex, Promise) {
+  return knex.transaction(function (trx) {
+    return trx.from('mentions').pluck('url').then(function (urls) {
+        var updates = [];
+
+        urls.forEach(function (url) {
+          var update = trx.table('mentions')
+            .where('url', url)
+            .update('normalizedUrl', urlTools.normalizeUrl(url));
+
+          updates.push(update);
+        });
+
+        return Promise.all(updates);
+      });
+    });
+};
