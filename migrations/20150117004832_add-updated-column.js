d9fffefd9e615b7fd7d83df3d5b00ed733d39324
Pelle Wessman <pelle@kodfabrik.se>
2015-01-17 00:53:11 +0100

    feat(parser): update existing sources on new ping

---
+++ migrations/20150117004832_add-updated-column.js
@@ -0,0 +1,36 @@
+'use strict';
+
+exports.up = function (knex, Promise) {
+  return knex.transaction(function (trx) {
+    return trx.schema.table('entries', function (table) {
+        table.timestamp('updated', true);
+      })
+      .then(function () {
+        return trx.from('entries');
+      })
+      .then(function (entries) {
+        var updates = [];
+
+        entries.forEach(function (entry) {
+          var update = trx.table('entries')
+            .where('normalizedUrl', entry.normalizedUrl)
+            .update({
+              updated: entry.fetched,
+            });
+
+          updates.push(update);
+        });
+
+        return Promise.all(updates);
+      });
+    })
+    .then(function () {
+      return knex.raw('ALTER TABLE "entries" ALTER COLUMN "updated" SET NOT NULL');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table('entries', function (table) {
+    table.dropColumn('updated');
+  });
+};
