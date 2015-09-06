da1bf5ac5115bffd3cfbe40a74aa9dfea0943903
Pelle Wessman <pelle@kodfabrik.se>
2015-09-06 15:09:27 +0200

    tests(main): added test for deep salmentions

--- test/functional/api.spec.js
+++ test/functional/api.spec.js
@@ -742,6 +742,89 @@ describe('WebMention API', function () {
       });
     });
 
+    it('should fetch and ping upstream salmention targets on downstream mention', function () {
+      var templateMock = nock('http://example.com')
+        .get('/')
+        .once()
+        .reply(200, function() {
+          return '<div class="h-entry">' +
+            '<a href="http://example.net/foo">First</a>' +
+            '<a class="u-comment" href="http://example.com/foo">First</a>' +
+          '</div>';
+        })
+        .get('/foo')
+        .once()
+        .reply(200, function() {
+          return '<div class="h-entry">' +
+            '<a href="http://example.com/">First</a>' +
+          '</div>';
+        });
+
+      var targetMock = nock('http://example.net')
+        .get('/foo')
+        .twice() //TODO: Should be .once() really
+        .reply(200, function() {
+          return '<div class="h-entry">' +
+            '<a class="u-in-reply-to" href="http://example.net/bar">First</a>' +
+          '</div>';
+        })
+        .get('/bar')
+        .twice() //TODO: Should be .once() really
+        .reply(200, function() {
+          return '<html><head>' +
+            '<link rel="webmention" href="http://webmention.example.com/ping" />' +
+          '</head><body>' +
+              '<div class="h-entry">a simple linkless entry</div>' +
+          '</html>';
+        });
+
+      var pingMock = nock('http://webmention.example.com')
+        .post('/ping', {
+          source: 'http://example.net/foo',
+          target: 'http://example.net/bar',
+        })
+        .twice() //TODO: Should be .once() really
+        .reply(202);
+
+      return new Promise(function (resolve, reject) {
+        request(app)
+          .post('/api/webmention')
+          .send({
+            source: 'http://example.com/',
+            target: 'http://example.net/foo',
+          })
+          .expect(202)
+          .end(function (err) {
+            if (err) {
+              return reject(err);
+            }
+            resolve();
+          });
+      })
+      .then(waitForNotification(4))
+      .then(function () {
+        //TODO: Improve – relyng on timers in tests are pretty fragile
+        return new Promise(function (resolve) {
+          setTimeout(resolve, 300);
+        });
+      })
+      .then(function () {
+        return Promise.all([
+          knex('entries').count('id').first(),
+          knex('mentions').count('eid').first(),
+        ]);
+      })
+      .then(function (result) {
+        templateMock.done();
+        targetMock.done();
+        pingMock.done();
+        result.should.deep.equal([
+          {count: '4'},
+          {count: '2'},
+        ]);
+      });
+    });
+
   });
 
   describe('fetch mentions', function () {
