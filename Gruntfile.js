81e0c23ca788310ea7555ea45ea9b98cbc698a16
Pelle Wessman <pelle@kodfabrik.se>
2018-05-12 21:13:51 +0200

    Modernizing test setup - less Grunt, more npm

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,47 +9,6 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    eslint: {
-      src: [
-        'Gruntfile.js',
-        'lib/**/*.js',
-        'migrations/**/*.js',
-        'theme/*.js',
-        'theme/sources/js/**/*.js',
-        '!theme/sources/js/vendor/**/*.js',
-        'test/**/*.js'
-      ]
-    },
-    lintspaces: {
-      files: ['<%= eslint.src %>'],
-      options: { editorconfig: '.editorconfig' }
-    },
-    mocha_istanbul: {
-      options: {
-        root: './lib',
-        mask: '**/*.spec.js'
-      },
-      unit: {
-        src: 'test/unit'
-      },
-      basic: {
-        src: 'test'
-      },
-      coveralls: {
-        src: 'test',
-        options: {
-          coverage: true,
-          reportFormats: ['lcovonly']
-        }
-      }
-    },
-    'dependency-check': {
-      files: ['<%= eslint.src %>'],
-      options: {
-        excludeUnusedDev: true,
-        ignoreUnused: ['pg']
-      }
-    },
     concat: {
       options: {
         separator: ';'
@@ -97,34 +56,12 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-notify');
-  grunt.loadNpmTasks('grunt-lintspaces');
-  grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-istanbul');
-  grunt.loadNpmTasks('dependency-check');
-
-  grunt.registerTask('setTestEnv', 'Ensure that environment (database etc) is set up for testing', function () {
-    process.env.NODE_ENV = 'test';
-  });
 
   grunt.registerTask('build-dev', ['concat']);
   grunt.registerTask('build', ['concat', 'uglify']);
 
-  grunt.registerTask('basic-test', ['lintspaces', 'eslint', 'dependency-check', 'setTestEnv']);
-  grunt.registerTask('travis', ['basic-test', 'mocha_istanbul:coveralls']);
-  grunt.registerTask('test', ['basic-test', 'mocha_istanbul:basic']);
-  grunt.registerTask('fast-test', ['basic-test', 'mocha_istanbul:unit']);
-
-  grunt.registerTask('default', ['build-dev', 'test']);
-
-  grunt.event.on('coverage', function (lcov, done) {
-    require('coveralls').handleInput(lcov, function (err) {
-      if (err) {
-        return done(err);
-      }
-      done();
-    });
-  });
+  grunt.registerTask('default', ['build-dev']);
 };
