91731cecbf99b9f25d985d1adf7f2ec7c240d72c
Pelle Wessman <pelle@kodfabrik.se>
2018-09-13 19:27:17 +0200

    Fix database connections

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const pathModule = require('path');
+
 const { db: connection } = require('./lib/config');
+
 const db = {
   client: 'pg',
-  connection
+  connection,
+  migrations: {
+    install: pathModule.join(__dirname, './lib/install-schema'),
+    directory: pathModule.join(__dirname, './migrations')
+  }
 };
 
 module.exports = {
